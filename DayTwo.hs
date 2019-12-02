ba515c3c53dbe2ec1bff111b9a0fbb338481ea8b
Trevor Riles <trevor@trevorriles.com>
2019-12-02 07:45:41 -0600

    Day two complete
    Used a pretty gross brute force method for part two. Also no idea what I'm doing with lenses :D

---
+++ DayTwo.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TypeApplications #-}
+
+module DayTwo where
+
+import Data.List.Split (splitOn)
+import Control.Lens
+
+solve :: IO ()
+solve = do
+    input <- readFile "./day-two-input.txt"
+    let codes = read @Int <$> splitOn "," input
+    let codes' = codes & element 1 .~ 12 & element 2 .~ 2
+    putStrLn $ show $ compute codes' 0
+
+compute :: [Int] -> Int -> [Int]
+compute input pos = do
+    let cmd = input ^?! element pos
+    case cmd of
+        99 -> input
+        1 -> do
+            let in1 = input ^?! element (input ^?! element (pos + 1))
+            let in2 = input ^?! element (input ^?! element (pos + 2))
+            let out = input ^?! element (pos + 3)
+            compute (input & element out .~ (in1 + in2)) (pos + 4)
+        2 -> do
+            let in1 = input ^?! element (input ^?! element (pos + 1))
+            let in2 = input ^?! element (input ^?! element (pos + 2))
+            let out = input ^?! element (pos + 3)
+            compute (input & element out .~ (in1 * in2)) (pos + 4)
+
+
+solve2 :: Int -> Int -> IO ()
+solve2 noun verb = do
+    input <- readFile "./day-two-input.txt"
+    let codes = read @Int <$> splitOn "," input
+    let codes' = codes & element 1 .~ noun & element 2 .~ verb
+    let output = compute codes' 0 ^?! element 0
+    if output == 19690720 then 
+        putStrLn $ show (100 * noun + verb)
+    else
+        if verb < 99 then
+            solve2 noun (verb + 1)
+        else if noun < 99 then
+            solve2 (noun + 1) 0
+        else
+            putStrLn "Failure to solve puzzle"
