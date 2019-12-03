600b455279b54dbae4eb52dab8bd6f47d3065c48
trevorriles <tjriles@spscommerce.com>
2019-12-03 07:09:07 -0600

    Day3 part 2 complete

--- src/DayThree.hs
+++ src/DayThree.hs
@@ -3,6 +3,7 @@
 module DayThree where
 
 import qualified Data.Set as S
+import qualified Data.Map as M
 import Data.List.Split (splitOn)
 import Data.Function ((&))
 
@@ -49,3 +50,33 @@ addCoords state@((x, y) : _) (Instruction d n) = do
         R -> addCoords ((x + 1, y):state) (Instruction d $ n - 1)
         L -> addCoords ((x - 1, y):state) (Instruction d $ n - 1)
 addCoords _ _ = error "Bad Input"
+
+
+solve2 :: IO ()
+solve2 = do
+    input <- readFile "./src/day-three-input.txt"
+    let (x:y:[]) = lines input
+    let result = solve2' x y
+    putStrLn . show $ result
+
+solve2' :: String -> String -> Int
+solve2' in1 in2 = do
+    let (_, _, m1) = runInstructions2 $ parseInput in1
+    let (_, _, m2) = runInstructions2 $ parseInput in2
+    let latencyMap = M.intersectionWith (+) m1 m2
+    foldl (\x y -> if x <= y then x else y) 999999 latencyMap
+
+
+runInstructions2 :: [Instruction] -> (Int, (Int, Int), M.Map (Int, Int) Int)
+runInstructions2 = foldl addCoords2 (0, (0,0), mempty)
+
+
+addCoords2 :: (Int, (Int, Int), M.Map (Int, Int) Int) -> Instruction -> (Int, (Int, Int), M.Map (Int, Int) Int)
+addCoords2 state (Instruction _ 0) = state
+addCoords2 (steps, (x, y), state) (Instruction d n) = do 
+    let newSteps = steps + 1
+    case d of
+        U -> addCoords2 (newSteps, (x, y + 1), M.insert (x, y + 1) (newSteps) state) (Instruction d $ n - 1)
+        D -> addCoords2 (newSteps, (x, y - 1), M.insert (x, y - 1) (newSteps) state) (Instruction d $ n - 1)
+        R -> addCoords2 (newSteps, (x + 1, y), M.insert (x + 1, y) (newSteps) state) (Instruction d $ n - 1)
+        L -> addCoords2 (newSteps, (x - 1, y), M.insert (x - 1, y) (newSteps) state) (Instruction d $ n - 1)
